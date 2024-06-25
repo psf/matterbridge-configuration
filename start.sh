099868bb00499b593c325dc1d170d849bb91cd2b
Ee Durbin <ewdurbin@gmail.com>
2024-06-25 10:57:43 -0400

    fix handling of term signals

--- start.sh
+++ start.sh
@@ -5,25 +5,7 @@
 ###################################################################
 envsubst < /matterbridge/config-template.toml > /matterbridge/config.toml
 
-#####################################
-# Pass SIGTERM to Matterbridge proc #
-#####################################
-function _term {
-  echo "Sending SIGTERM to matterbridge"
-
-  kill --TERM "$PID" 2>/dev/null
-}
-
-trap _term SIGTERM
-
 ####################
 # Start Matterbridge #
 ####################
-matterbridge -conf /matterbridge/config.toml &
-
-PID=$!
-
-#####################################
-# Wait for this process to complete #
-#####################################
-wait "$PID"
+/bin/matterbridge -conf /matterbridge/config.toml
